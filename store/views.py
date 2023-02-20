231d2fcfca18b3710faf9e1cce7af8b3fcb27714
sammwaura <meshsami27@gmail.com>
2023-02-20 14:29:09 +0300

    process order data

--- store/views.py
+++ store/views.py
@@ -76,4 +76,9 @@ def updateItem(request):
 
     return JsonResponse('Item was added', safe=False)
 
+def processOrder(request):
+    print('Data:', request.body)
+    return JsonResponse('Payment Complete!', safe=False)
+
+
 
