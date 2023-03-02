6dfb2dc907e2307a65e5df86c2a6ba5197d18e37
sammwaura <meshsami27@gmail.com>
2023-03-02 13:14:23 +0300

    modify payment of mpesa and paypal

--- store/views.py
+++ store/views.py
@@ -84,6 +84,7 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        
         total = float(data['form']['total'])
         order.transaction_id = transaction_id
 
